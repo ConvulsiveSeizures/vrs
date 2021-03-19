b51e6a89f5e3c726ac672b31ebf20c1787a216a9
Seizure <78146652+ConvulsiveSeizure@users.noreply.github.com>
2021-03-19 18:40:03 +0300

    addsomechanges

--- main.js
+++ main.js
@@ -2,4 +2,12 @@ $(document).ready(() => {
     setTimeout(() => {
         $("#themeWrapper").addClass("changeTheme")
     }, 100)
+
+    $("#closeContacts").on("click", () => {
+        $("#contacts").removeClass("active")
+    })
+
+    $("#showContacts").on("click", () => {
+        $("#contacts").addClass("active")
+    })
 })
